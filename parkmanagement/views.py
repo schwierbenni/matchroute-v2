8d8a57e358075c3a5069021b1ea625cc5bf73428
Benedict Schwier <schwier.b@gmail.com>
2025-04-03 15:09:53 +0200

    BS3-enabled token auth

--- parkmanagement/views.py
+++ parkmanagement/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from .models import Parkplatz
 from .serializers import ParkplatzSerializer, UserRegisterSerializer
@@ -7,6 +8,7 @@ from .serializers import ParkplatzSerializer, UserRegisterSerializer
 class ParkplatzViewSet(viewsets.ModelViewSet):
     queryset = Parkplatz.objects.all()
     serializer_class = ParkplatzSerializer
+    #permission_classes = [IsAuthenticated]
 
 class UserRegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
